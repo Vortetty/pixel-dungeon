98993a813a5bc837dd53832ef10b1e94345e99e9
Britsman <eric_britsman@hotmail.com>
2014-11-11 13:35:19 +0100

    gui

--- src/com/dit599/customPD/CustomPD.java
+++ src/com/dit599/customPD/CustomPD.java
@@ -294,7 +294,7 @@ public class CustomPD extends Game {
 	}
 
 	public void editMaps() {
-		startActivity(new Intent ("com.dit599.customPD.editorUI.FloorsListActivity"));
+		startActivity(new Intent ("com.dit599.customPD.editorUI.MapEditorMainActivity"));
 	}
 	
 
