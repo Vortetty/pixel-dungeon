73586c7f765b93a0f1dd9b694235fc2d56c06028
Britsman <eric_britsman@hotmail.com>
2014-11-11 11:42:31 +0100

    Added necessary tags to certain strings so it is clear in highscore etc that you are playing CustomPD and not the original game.

--- src/com/dit599/customPD/scenes/RankingsScene.java
+++ src/com/dit599/customPD/scenes/RankingsScene.java
@@ -39,7 +39,7 @@ import com.watabou.noosa.ui.Button;
 
 public class RankingsScene extends PixelScene {
 	
-	private static final String TXT_TITLE		= "Top Rankings";
+	private static final String TXT_TITLE		= "CustomPD Top Rankings";
 	private static final String TXT_TOTAL		= "Total games played: %d";
 	private static final String TXT_NO_GAMES	= "No games have been played yet.";
 	
