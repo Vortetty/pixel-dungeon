b498ce750d0507898caa5084c2606c2bb2d40447
Eric Britsman <eric_britsman@hotmail.com>
2014-12-08 20:34:03 +0100

    Mapselector based on real files.

--- src/com/dit599/customPD/editorUI/MapSelectorActivity.java
+++ src/com/dit599/customPD/editorUI/MapSelectorActivity.java
@@ -1,8 +1,11 @@
 package com.dit599.customPD.editorUI;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -10,85 +13,58 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.dit599.customPD.R;
+import com.watabou.noosa.Game;
 
 public class MapSelectorActivity extends Activity {
-	private Button mapbu1,mapbu2,mapbu3;
 	public EditText edv=null;
 	public Button bt=null;
 	public LinearLayout mlayout=null;
-	public static String choosemap=null;
-	
-	
+	ArrayList<String> files;
+
+
 	@Override  
-    public void onCreate(Bundle savedInstanceState) {  
-        super.onCreate(savedInstanceState);  
-        setContentView(R.layout.mapselect);  
-        edv=(EditText)findViewById(R.id.mapselectet);
-        bt=(Button)this.findViewById(R.id.mapselectbut);
-        mlayout= (LinearLayout) findViewById(R.id.mapselectlinear);
-        mapbu1=(Button) findViewById(R.id.mapex1bu);
-        mapbu2=(Button) findViewById(R.id.mapex2bu);
-        mapbu3=(Button) findViewById(R.id.mapex3bu);
+	public void onCreate(Bundle savedInstanceState) {  
+		super.onCreate(savedInstanceState);  
+		setContentView(R.layout.mapselect);  
+		edv=(EditText)findViewById(R.id.mapselectet);
+		bt=(Button)this.findViewById(R.id.mapselectbut);
+		files = new ArrayList<String>();
+
+		for(String f : Game.instance.fileList()){
+			if(f.endsWith(".map")){
+				files.add(f.substring(0, f.length()-4));
+				Button but=new Button (MapSelectorActivity.this);
+				but.setOnClickListener(getListener());
+				but.setText(f.substring(0, f.length()-4));
+				mlayout.addView(but);
+			}
+		}
+		mlayout= (LinearLayout) findViewById(R.id.mapselectlinear);
 		bt.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-			   
-			    Button but=new Button (MapSelectorActivity.this);
-			    but.setOnClickListener(new OnClickListener(){
+				String temp = edv.getText().toString();
+				if(!temp.equals("") && !files.contains(temp)){
+					Button but=new Button (MapSelectorActivity.this);
+					but.setOnClickListener(getListener());
+					but.setText(temp);
+					mlayout.addView(but);
 
-					@Override
-					public void onClick(View v) {
-						// TODO Auto-generated method stub
-						Intent intent = new Intent();  
-		                intent.setClass(MapSelectorActivity.this, MapEditActivity.class);  
-                        intent.putExtra(MapEditActivity.EXTRA_FILENAME, "map1");
-		                startActivity(intent);  
-					}});
-			
-			    but.setText(edv.getText().toString()+".map");
-			    choosemap=but.getText().toString();
-			    mlayout.addView(but);
-				
-			}});
-		mapbu1.setOnClickListener(new OnClickListener(){
-
-		
-
-					@Override
-					public void onClick(View v) {
-						// TODO Auto-generated method stub
-						Intent intent = new Intent();  
-		                intent.setClass(MapSelectorActivity.this, MapEditActivity.class);  
-		                intent.putExtra(MapEditActivity.EXTRA_FILENAME, "map1");
-		                startActivity(intent);  
-					}});
-		mapbu2.setOnClickListener(new OnClickListener(){
-
-			
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent intent = new Intent();  
-                intent.setClass(MapSelectorActivity.this, MapEditActivity.class);  
-                startActivity(intent);  
-			}});
-		mapbu3.setOnClickListener(new OnClickListener(){
-
-			
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent intent = new Intent();  
-                intent.setClass(MapSelectorActivity.this, MapEditActivity.class);  
-                startActivity(intent);  
-			}});
-			
-			   
-        
+				}}});
 	}     
+	private OnClickListener getListener(){
+		return new OnClickListener(){
 
+			@Override
+			public void onClick(View v) {
+				String temp = ((Button)v).getText().toString();
+				Intent intent = new Intent();  
+				intent.setClass(MapSelectorActivity.this, MapEditActivity.class);  
+				intent.putExtra(MapEditActivity.EXTRA_FILENAME, temp);
+				Log.d("NAME PARAM", temp);
+				startActivity(intent);  
+			}};
+	}
 }
