3ef2cff6f8584be9db98e417ca4bd90f0afad46b
Calambor <antongr@student.chalmers.se>
2014-12-12 15:31:54 +0100

    Removed unused code

--- src/com/dit599/customPD/editorUI/SuperTreeNode.java
+++
@@ -1,9 +0,0 @@
-package com.dit599.customPD.editorUI;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class SuperTreeNode {
-	public Object parent;
-	public List<TreeViewAdapter.TreeNode> childs = new ArrayList<TreeViewAdapter.TreeNode>();
-}
